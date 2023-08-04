4d1c7ad71700da724be35ffb2e163d029da87899
Lucas Martins <martinslf@taginfraestrutura.com.br>
2023-08-04 14:44:45 -0300

    first commit

---
+++ ConvertJsonToCsv/Program.cs
@@ -0,0 +1,33 @@
+﻿using ChoETL;
+
+var pathBase = "C:\\ConvertJsonToCsv";
+if (!Directory.Exists(pathBase))
+{
+    Directory.CreateDirectory(pathBase);
+}
+var choice = "s";
+do
+{
+    Console.WriteLine("Informe o nome do arquivo");
+    var filename = Console.ReadLine();
+
+
+    var pathJson = $"{pathBase}\\{filename}.json";
+    var pathCsv = $"{pathBase}\\{filename}.csv";
+
+
+    using (var r = new ChoJSONReader(pathJson))
+    {
+        using (var w = new ChoCSVWriter(pathCsv).WithFirstLineHeader())
+        {
+            w.Write(r);
+        }
+    }
+
+    Console.WriteLine("Arquivo gerado"); 
+    
+    Console.WriteLine("Deseja converter outro arquivo? (s/n)");
+    choice = Console.ReadLine() ?? "n";
+} while (choice.ToLower() == "s");
+
+Console.WriteLine("bye");
